c400be21951182447ff613050a905d16f8c8a3d9
Sai <saibhagatsr@gmail.com>
2025-08-17 20:50:40 +0800

    added Level 1

--- src/main/java/Chalk.java
+++ src/main/java/Chalk.java
@@ -1,8 +1,35 @@
+
+import java.util.Scanner;
+
 public class Chalk {
+
+    private static final String NAME = "Chalk";
+
+    private static final String LINE_BREAK = "-------------------------------";
+    private static final String END_CONVERSATION = "bye";
+
     public static void main(String[] args) {
-        String name = "Chalk";
-        System.out.println("Hello! I'm " + name);
+
+        System.out.println("Hello! I'm " + Chalk.NAME);
         System.out.println("What can I do for you?");
+        System.out.println(Chalk.LINE_BREAK);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine();
+            System.out.println(Chalk.LINE_BREAK);
+
+            if (userInput.equals(END_CONVERSATION)) {
+                break;
+            }
+
+            System.out.println("Chalk: " + userInput);
+            System.out.println(Chalk.LINE_BREAK);
+        }
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(Chalk.LINE_BREAK);
+        scanner.close();
     }
 }
