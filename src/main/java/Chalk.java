a7d6a0609fba1800d1cd8ba3819a364e5222179f
Sai <saibhagatsr@gmail.com>
2025-08-17 19:43:06 +0800

    added level 0 functionality!

--- src/main/java/Chalk.java
+++ src/main/java/Chalk.java
@@ -1,6 +1,8 @@
 public class Chalk {
     public static void main(String[] args) {
-        String logo = "Chalk";
-        System.out.println("Hello from " + logo);
+        String name = "Chalk";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
